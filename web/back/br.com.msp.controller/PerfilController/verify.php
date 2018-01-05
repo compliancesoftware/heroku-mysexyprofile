84e0e25b02bb94b6c36580e42e61d306205c4641
Douglas Fernandes <br.com.age@gmail.com>
2018-01-05 10:11:28 -0300

    [MySexyProfile v1.0] - Douglas Fernandes: Disponibilidade de atributos sem consultar serviço

--- web/back/br.com.msp.controller/PerfilController/verify.php
+++ web/back/br.com.msp.controller/PerfilController/verify.php
@@ -7,7 +7,34 @@
     
     $service = new PerfilService();
 
-    $logado = $service->retrievePerfil();
+    // $logado = $service->retrievePerfil();
+
+    $logado = new Perfil();
+    $logado->setNome('Keyla Sales');
+    $logado->setSenha('<secret>');
+    $logado->setEmail('keyla.sales@gmail.com');
+
+    $cover = base64_encode(file_get_contents('../../../resources/images/cover/cover.jpg'));
+
+    $logado->setCapa($cover);
+
+    $icon = base64_encode(file_get_contents('../../../resources/images/profile/profile.jpg'));
+
+    $logado->setIcone($icon);
+    $logado->setCidade('Recife');
+    $logado->setEstado('Pernambuco');
+    $logado->setLatitude('-8.063583');
+    $logado->setLongitude('-34.871192');
+    $logado->setIdade(26);
+    $logado->setSigno('Peixes');
+    $logado->setAltura(1.71);
+    $logado->setPeso(69.0);
+    $atributos = 'Modelo Panicat;Anal;Oral;Massagem Tantrica;Lisinha;69;'.
+                      'Final Feliz Oral;Final Feliz Anal;Menage Masculino;Menage Feminino;'.
+                      'Festinha com casal(ele/ele ou ele/ela ou ela/ela);'.
+                      'Orgia;Namoradinha;Secretária executiva (fetiche);GangBang;Swing;Glory Hole';
+    $logado->setAtributos($atributos);
     
-    echo $logado;
+    echo $logado->serialize();
+    // echo $logado;
 ?>
